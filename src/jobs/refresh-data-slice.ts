777b5bf3ef79b0c1a4b6c1c7b0980801ed2ddcb4
Mirai Akagawa <mirai418@me.com>
2022-09-27 18:34:33 +0900

    making data refresh much faster

--- src/jobs/refresh-data-slice.ts
+++ src/jobs/refresh-data-slice.ts
@@ -7,34 +7,48 @@ import Region from '../models/region.js';
 import Seat from '../models/seat.js';
 import Status from '../models/status.js';
 import { stringify } from '../utils/printer.js';
+import sequelize from '../config/sequelize.js';
 
 function createUrl(seat: Seat, region: Region, status: Status) {
   const date = new Date('2022-09-01').getTime();
   return `https://cam.ana.co.jp/amctop/CAL_${seat.lookupId}_${region.lookupId}_${status.lookupId}.js?_=${date}`;
 }
 
-async function conditionalInsert(condition: WhereOptions<AwardSeat>, availabilityId: number) {
-  try {
-    const latest = await AwardSeat.findOne({
-      where: condition,
-    });
-    if (!isNil(latest) && latest.availabilityId !== availabilityId) {
-      console.log(`--> refresh-data-slice:conditionalInsert | found update on Award Seat: ${await stringify(latest)}`);
-      await latest.update({
-        isLatest: false,
-      });
-    }
-    if (isNil(latest) || latest.availabilityId !== availabilityId) {
-      console.log(`--> refresh-data-slice:conditionalInsert | inserting data ${await stringify(<AwardSeat>condition)} with availability_id=${availabilityId}`);
-      const insert = {
+async function conditionalInsert(dates: string[], availabilityIds: string[], condition: WhereOptions<AwardSeat>) {
+  const latests = await AwardSeat.findAll({
+    where: condition,
+  });
+  const dateLatestMap = new Map();
+  latests.forEach((latest) => {
+    const date = new Date(latest.date).getTime();
+    dateLatestMap.set(date, latest);
+  });
+  for (let i = 0; i < dates.length; i++) {
+    const date = new Date(dates[i]);
+    const availabilityId = parseInt(availabilityIds[i]);
+    try {
+      const latest = dateLatestMap.get(date.getTime());
+      const toBeCreated = <AwardSeat>{
         ...condition,
+        date: date,
         availabilityId: availabilityId,
       };
-      await AwardSeat.create(<AwardSeat>insert);
+      if (isNil(latest)) {
+        const awardSeat = await AwardSeat.create(toBeCreated);
+        console.log(`--> refresh-data-slice:conditionalInsert | new Award Seat ${await stringify(awardSeat)}`);
+      } else if (latest.availabilityId !== availabilityId) {
+        const awardSeat = await sequelize.transaction(async (t) => {
+          await latest.update({
+            isLatest: false,
+          }, {transaction: t});
+          return await AwardSeat.create(toBeCreated, {transaction: t});
+        });
+        console.log(`--> refresh-data-slice:conditionalInsert | updated Award Seat: ${await stringify(awardSeat)}`);
+      }
+    } catch (error) {
+      console.log(`!!! refresh-data-slice:conditionalInsert | failed to update data ${await stringify(<AwardSeat>condition)} with date=${date} and availability_id=${availabilityId}`);
+      console.log(error);
     }
-  } catch (error: any) {
-    console.log(`!!! refresh-data-slice:conditionalInsert | failed to insert data ${await stringify(<AwardSeat>condition)} with availability_id=${availabilityId}`);
-    console.log(error.stack);
   }
 }
 
@@ -55,21 +69,17 @@ async function insertData(data: any[], status: Status, seat: Seat, region: Regio
   const dates = data.shift();
   dates.shift();
   dates.shift();
-  await Promise.all(data.map(async (row) => {
-    const routeFrom = row.shift();
-    const routeTo = row.shift();
-    for (let i = 0; i < row.length; i++) {
-      const availabilityId = parseInt(row[i]);
-      await conditionalInsert({
-        date: new Date(dates[i]),
-        routeFrom: routeFrom,
-        routeTo: routeTo,
-        regionId: region.id,
-        statusId: status.id,
-        seatId: seat.id,
-        isLatest: true,
-      }, availabilityId);
-    }
+  await Promise.all(data.map(async (availabilityIds) => {
+    const routeFrom = availabilityIds.shift();
+    const routeTo = availabilityIds.shift();
+    conditionalInsert(dates, availabilityIds, {
+      routeFrom: routeFrom,
+      routeTo: routeTo,
+      regionId: region.id,
+      statusId: status.id,
+      seatId: seat.id,
+      isLatest: true,
+    });
   }));
 }
 
