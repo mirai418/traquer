b17bceb66c14a4a51d86dab6e6bf0705232d273d
Mirai Akagawa <mirai418@me.com>
2022-11-04 17:14:00 -0400

    fixing controller interface to include next function

--- src/controllers/controller-interface.ts
+++ src/controllers/controller-interface.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 interface Controller {
   path: string,
   router: express.Router,
   initializeRoutes(): void,
-  get?(request: express.Request, response: express.Response): void,
-  post?(request: express.Request, response: express.Response): void,
-  patch?(request: express.Request, response: express.Response): void,
-  delete?(request: express.Request, response: express.Response): void,
+  get?(request: Request, response: Response, next: NextFunction): void,
+  post?(request: Request, response: Response, next: NextFunction): void,
+  patch?(request: Request, response: Response, next: NextFunction): void,
+  delete?(request: Request, response: Response, next: NextFunction): void,
 }
 
 export default Controller;
