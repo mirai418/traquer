84f0f74dcf6d28f314fb32c70816e208155edb20
Mirai Akagawa <mirai418@me.com>
2022-11-17 17:22:29 +0900

    send updates found email to each user for their subscriptions

--- src/jobs/check-subscriptions.ts
+++ src/jobs/check-subscriptions.ts
@@ -1,15 +1,16 @@
 import { Op } from 'sequelize';
-import { isNil, isPlainObject, isArray, omitBy } from 'lodash-es';
+import { isNil, isPlainObject, isArray, omitBy, groupBy, orderBy } from 'lodash-es';
 
 import AwardSeat from '../models/award-seat.js';
 import Subscription from '../models/subscription.js';
-import { stringify } from '../utils/printer.js';
+import { send } from '../utils/mailer.js';
+import User from '../models/user.js';
 
 function isNilOrEmpty<T>(val: T): boolean {
   return (isArray(val) || isPlainObject(val)) ? (Object.keys(<[] | object>val).length + Object.getOwnPropertySymbols(val).length) === 0 : isNil(val);
 }
 
-async function checkSubscription(subscription: Subscription) {
+async function getUpdates(subscription: Subscription): Promise<AwardSeat[]> {
   const whereOptions = omitBy({
     routeFrom: subscription.routeFrom,
     routeTo: subscription.routeTo,
@@ -29,36 +30,56 @@ async function checkSubscription(subscription: Subscription) {
     where: whereOptions,
     order: [['date', 'ASC']],
   });
-  const subscriptionText = `${updates.length} update(s) found on Subscription: ` + await stringify(subscription);
-  console.log(`--> checkSubscriptions:checkSubscriptions | ${subscriptionText}`);
-  const updateTexts = [];
-  for (let i = 0; i < updates.length; i++) {
-    const updateText = await stringify(updates[i]);
-    console.log(`--> checkSubscriptions:checkSubscriptions | ` + updateText);
-    updateTexts.push(updateText);
-  }
   await subscription.update({
     lastCheckedAt: new Date(),
   });
-  return {
-    subscription: subscriptionText,
-    updates: updateTexts,
-  };
+  return updates;
 }
+
+async function humanizeUpdates(updates: AwardSeat[]) {
+  const res = [];
+  for (let i = 0; i < updates.length; i++) {
+    const update = updates[i];
+    res.push({
+      availabilityId: update.availabilityId,
+      date: update.date,
+      humanized: await update.humanize(),
+    });
+  }
+  return groupBy(orderBy(res, 'date'), 'availabilityId');
+}
+
 
 async function checkSubscriptions() {
   const startTime = new Date();
   console.log(`--> checkSubscriptions | starting: ${startTime} (${startTime.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })} JST).`);
-  const subscriptions = await Subscription.findAll();
-  const results = [];
-  for (let i = 0; i < subscriptions.length; i++) {
-    results.push(await checkSubscription(subscriptions[i]));
+  let totalUpdatesCount = 0;
+  const users = await User.findAll();
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    const subscriptions = await Subscription.findAll({
+      where: {
+        userId: user.id,
+      },
+    });
+    let updates: AwardSeat[] = [];
+    for (let i = 0; i < subscriptions.length; i++) {
+      updates = updates.concat(await getUpdates(subscriptions[i]));
+    }
+    if (updates.length > 0) {
+      totalUpdatesCount += updates.length;
+      const sanitizedUpdates = await humanizeUpdates(updates);
+      send(user.email, '[traquer] New Updates on Award Seats', 'updates-found-template', {
+        user: user,
+        updatesCount: updates.length,
+        updates: sanitizedUpdates,
+      });
+    }
   }
-
   const endTime = new Date();
   const duration = endTime.getTime() - startTime.getTime();
   console.log(`--> checkSubscriptions | done: ${endTime} (${endTime.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })} JST). Took: ${duration}ms.`);
-  return results;
+  return totalUpdatesCount;
 }
 
 export default checkSubscriptions;
