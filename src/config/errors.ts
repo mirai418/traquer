c1c71cd6ce78201793e2563e74a59feacde5f1d3
Mirai Akagawa <mirai418@me.com>
2022-11-18 16:06:56 +0900

    remove errors.ts as no longer needed

--- src/config/errors.ts
+++
@@ -1,6 +0,0 @@
-export class ForbiddenError extends Error {}
-export class NotUpdatableFieldError extends Error {}
-export class NotAdminError extends Error {}
-export class NotFoundError extends Error {}
-export class NoUserError extends Error {}
-export class TokenEmptyError extends Error {}
