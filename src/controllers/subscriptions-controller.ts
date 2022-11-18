85d4fa6e56788067d84fae6d51e9c928c4c87905
Mirai Akagawa <mirai418@me.com>
2022-11-18 16:05:04 +0900

    migrating SubscriptionsController to use errorHandler

--- src/controllers/subscriptions-controller.ts
+++ src/controllers/subscriptions-controller.ts
@@ -1,39 +1,16 @@
 import express, { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
 
 import Controller from './controller-interface.js';
 import Subscription from '../models/subscription.js';
 import User from '../models/user.js';
 import { authMiddleware, RequestWithUser } from '../middlewares/auth-middleware.js';
-import { ValidationError } from 'sequelize';
-import { ForbiddenError, NotFoundError, NotUpdatableFieldError } from '../config/errors.js';
 import { clone } from 'lodash-es';
 
 const includeUserObj = {
   model: User,
   as: 'user',
 };
-
-function handleError(error: unknown, response: Response, next: NextFunction) {
-  if (error instanceof NotFoundError) {
-    response.status(404).json({
-      message: 'Not Found. Could not find this resource.',
-    });
-  } else if (error instanceof ForbiddenError) {
-    response.status(403).json({
-      message: 'Forbidden. You do not have the rights to perform this action.',
-    });
-  } else if (error instanceof NotUpdatableFieldError) {
-    response.status(403).json({
-      message: 'Forbidden. This field is not assignable or updatable (by you).',
-    });
-  } else if (error instanceof ValidationError) {
-    response.status(401).json({
-      message: error.message,
-    });
-  } else {
-    next(error);
-  }
-}
 
 class SubscriptionController implements Controller {
   public path = '/subscriptions';
@@ -69,7 +46,7 @@ class SubscriptionController implements Controller {
         response.json(result);
       }
     } catch (error) {
-      handleError(error, response, next);
+      next(error);
     }
   }
 
@@ -77,13 +54,13 @@ class SubscriptionController implements Controller {
     try {
       const user = (request as RequestWithUser).user;
       if (!user.isAdmin && (request.body.userId !== user.id)) {
-        throw new NotUpdatableFieldError();
+        throw new createHttpError.Forbidden('You cannot create Subscriptions for other users.');
       } else {
         const result = await Subscription.create(request.body);
         response.json(result);
       }
     } catch (error) {
-      handleError(error, response, next);
+      next(error);
     }
   }
 
@@ -91,18 +68,18 @@ class SubscriptionController implements Controller {
     try {
       const user = (request as RequestWithUser).user;
       const subscription = await Subscription.findByPk(request.params.id);
-      if (!subscription) {
-        throw new NotFoundError();
-      } else if (!user.isAdmin && (subscription.userId !== user.id)) {
-        throw new ForbiddenError();
+      if (!user.isAdmin && (!subscription || subscription.userId !== user.id)) {
+        throw new createHttpError.Forbidden('You cannot update Subscriptions for other users.');
+      } else if (!subscription) {
+        throw new createHttpError.NotFound('Subscription not found.');
       } else if (!user.isAdmin && request.body.userId) {
-        throw new NotUpdatableFieldError();
+        throw new createHttpError.Forbidden('You cannot update the ownership of Subscriptions.');
       } else {
         await subscription.update(request.body);
         response.json(subscription);
       }
     } catch (error) {
-      handleError(error, response, next);
+      next(error);
     }
   }
 
@@ -110,10 +87,10 @@ class SubscriptionController implements Controller {
     try {
       const user = (request as RequestWithUser).user;
       const subscription = await Subscription.findByPk(request.params.id);
-      if (!subscription) {
-        throw new NotFoundError();
-      } else if (!user.isAdmin && (subscription.userId !== user.id)) {
-        throw new ForbiddenError();
+      if (!user.isAdmin && (!subscription || subscription.userId !== user.id)) {
+        throw new createHttpError.Forbidden('You cannot delete Subscriptions for other users.');
+      } else if (!subscription) {
+        throw new createHttpError.NotFound('Subscription not found.');
       } else {
         await subscription.destroy();
         response.json({
@@ -121,7 +98,7 @@ class SubscriptionController implements Controller {
         });
       }
     } catch (error) {
-      handleError(error, response, next);
+      next(error);
     }
   }
 
